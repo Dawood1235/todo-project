2daf37b927a0bf8eb8c88bd80c15c7d73d2b352f
Muhammad Dawood <dawoodiqbal137@gmail.com>
2026-09-11 16:47:10 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,13 @@ import UserManagement from "./pages/user-management";
 import ProtectedRoute from "./ProtectedRoute";
 // import { UserContext,  UserProvider } from "./ContextProvider";
 import PublicOnlyRoute from "./PublicOnlyRoute";
-import {auth} from './firebase';
-// import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import { getDatabase, ref, set } from 'firebase/database';
-// import { app } from './firebase';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Homepage from './pages/Index';
 import Details from './pages/Details';
 import AddData from "./pages/Add";
-import { FirebaseError } from "firebase/app";
 import Unauthorized from "./unauthorized";
 import Statistics from "./Statistics";
 import AdminDashboard from "./pages/Admin-dashboard";
-
-
-// const auth = getAuth(app);
-//<UserProvider>
 
 export default function App() {
 return(
@@ -106,33 +97,6 @@ element={<Unauthorized/>}
 );
 
 }
-
-// </UserProvider>
-//   return (<ContextProvider>
-//     <BrowserRouter>
-//   <Routes>
-
-//     {/* Public Routes */}
-//     <Route path="/pages/Signup" element={<SignUpPage />} />
-//     <Route path="/pages/Signin" element={<SignInPage />} />
-
-//     {/* Any logged-in user */}
-//     <Route path="/" element={
-//       <ProtectedRoute roles={[]}>
-//         <Homepage/>
-//        </ProtectedRoute>
-//     }/>
-
-//     {/* Admin only */}
-//     <Route path = "/pages/Add" element={
-//     <ProtectedRoute roles ={["admin"]}>
-//         <AddData/>
-//      </ProtectedRoute>
-//     }/>
-
-//   </Routes>
-// </BrowserRouter>
-// </ContextProvider>
   
 // export default App;
 
